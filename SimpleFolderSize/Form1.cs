df8fec57eb30fdb77d96274f840b0d0189b08b44
Swixs <miracleboyofficial1@gmail.com>
2022-04-16 17:48:25 +0300

    work

--- SimpleFolderSize/Form1.cs
+++ SimpleFolderSize/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +24,23 @@ namespace SimpleFolderSize
             DialogResult result = folderBrowserDialog1.ShowDialog();
             if(result != DialogResult.OK)
             {
+                FileInfo f;
 
+                long size = GetDirectorySize(new DirectoryInfo(folderBrowserDialog1.SelectedPath));
+                MessageBox.Show(size.ToString());
+
+                
             }
+        }
+        private long GetDirectorySize(DirectoryInfo directoryinfo)
+        {
+            long size = 0;
+            FileInfo[] allfiles = directoryinfo.GetFiles();
+            for (int i =0; i< allfiles.Length; i++)
+            {
+                size += allfiles[i].Length;
+            }
+            return size;
         }
     }
 }
